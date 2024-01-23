44e1c983606d11c80bf402aec3b56cc0656b51e1
ghodhbeni ahmed <ahmed.godhbeni@gmail.com>
2024-01-23 17:28:58 +0100

    working with strings commit

--- doc.rb
+++ doc.rb
@@ -16,10 +16,36 @@ puts "#{name} is a friendly individual who is #{age} years old."
 
 ### data types ###
 #basics
+=begin
 name = "Ahmed" #String
 age = 28 #Integer
 gpa = 3.2576 #Float
 isMale = true #Boolean
 flaws = nil #Nil=>Null
 puts "#{isMale}"
+=end
 
+### working with strings ###
+
+ruby_phrase = "Ruby is a powerful language for building web applications."
+my_phrase = "Hello from Tunisia!"
+white_spaces_phrase = "       super   "
+#upcase & downcase
+puts my_phrase.upcase()
+puts my_phrase.downcase()
+#strip
+puts white_spaces_phrase.strip()
+#length
+puts my_phrase.length()
+#include
+puts ruby_phrase.include? "Ruby"
+puts ruby_phrase.include? "Tunisia"
+#access
+puts my_phrase[0]
+puts my_phrase[18]
+puts my_phrase[-1]
+puts my_phrase[0, 5]
+#index
+puts my_phrase.index("f")
+#concat
+puts combined_string = ruby_phrase + my_phrase
