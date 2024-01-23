71655700f6cc67a6b0456a5418550736be6155eb
ghodhbeni ahmed <ahmed.godhbeni@gmail.com>
2024-01-23 17:04:21 +0100

    basic data types commit

--- doc.rb
+++ doc.rb
@@ -4,11 +4,22 @@
 #puts "hey" #println
 #print "hello"
 
-
 ### variables ###
 
 #Define variables for name and age
+=begin ---multiline comments---
 name = "Alice"
 age = 25
 #Display
 puts "#{name} is a friendly individual who is #{age} years old."
+=end
+
+### data types ###
+#basics
+name = "Ahmed" #String
+age = 28 #Integer
+gpa = 3.2576 #Float
+isMale = true #Boolean
+flaws = nil #Nil=>Null
+puts "#{isMale}"
+
