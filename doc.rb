c6db57795e2f86419886a3b12421d1dc5b5e1eea
ghodhbeni ahmed <ahmed.godhbeni@gmail.com>
2024-01-23 16:44:20 +0100

    variables commit

--- doc.rb
+++ doc.rb
@@ -1,5 +1,14 @@
 #documentation
 
 ### print & puts ###
-puts "hey" #println
-print "hello"
+#puts "hey" #println
+#print "hello"
+
+
+### variables ###
+
+#Define variables for name and age
+name = "Alice"
+age = 25
+#Display
+puts "#{name} is a friendly individual who is #{age} years old."
